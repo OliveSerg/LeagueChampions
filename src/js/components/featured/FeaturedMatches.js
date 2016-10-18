3e97438fa75bf6f4695fd78ed5b7c39e49704f5c
OliveSerg <oliveserg@gmail.com>
2016-10-18 12:38:13 -0400

    champion separated according to teams and placed into render/return

--- src/js/components/featured/FeaturedMatches.js
+++ src/js/components/featured/FeaturedMatches.js
@@ -7,31 +7,71 @@ export default class FeaturedMatches extends React.Component {
       super()
     }
 
-
-    render() {
-      const blueTeam = {
+    filterTeams(){
+      let filteredTeams = []
+      filteredTeams.push({
         bannedChampions: _.filter(this.props.bannedChampions, {teamId: 100}),
         participants: _.filter(this.props.participants, {teamId: 100})
-      }
-      const redTeam = {
+      })
+      filteredTeams.push({
         bannedChampions: _.filter(this.props.bannedChampions, {teamId: 200}),
         participants: _.filter(this.props.participants, {teamId: 200})
+      })
+      return filteredTeams
+    }
+
+    getChampionDiv(championsArray){
+      if (championsArray) {
+        return championsArray.map((champion)=>{
+          <Champion key={champion.championId + champion.teamId} {...champion}/>
+        })
       }
-      const BlueTeamChampions = blueTeam.participants.map((champion)=>{
-        <Champion key={champion.championId + champion.teamId} {...champion}/>
-      })
-      const RedTeamChampions = redTeam.participants.map((champion)=>{
-        <Champion key={champion.championId + champion.teamId} {...champion}/>
-      })
-      return(
+    }
+
+    render() {
+      const teams = this.filterTeams()
+      const blueTeam = teams[0]
+      const redTeam = teams[1]
+      console.log(teams);
+
+      const BlueTeamChampions = this.getChampionDiv(blueTeam.participants)
+      const BlueTeamBanChamps = this.getChampionDiv(blueTeam.bannedChampions)
+
+      const RedTeamChampions = this.getChampionDiv(redTeam.participants)
+      const RedTeamBanChamps = this.getChampionDiv(redTeam.bannedChampions)
+
+      if (this.props.bannedChampions) {
+        return(
           <div>
             <div>
               {BlueTeamChampions}
             </div>
             <div>
+              {BlueTeamBanChamps}
+            </div>
+
+            <div>
+              {RedTeamBanChamps}
+            </div>
+            <div>
               {RedTeamChampions}
             </div>
           </div>
-      )
+        )
+      } else {
+        return(
+          <div>
+            <div>
+              {BlueTeamChampions}
+            </div>
+            <div>
+
+            </div>
+            <div>
+              {RedTeamChampions}
+            </div>
+          </div>
+        )
+      }
     }
 }

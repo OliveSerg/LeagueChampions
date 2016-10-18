3c85e9d420c92b2e290c7a6242baee05af8b2646
OliveSerg <oliveserg@gmail.com>
2016-10-17 21:37:34 -0400

    feature teams fix

--- src/js/components/featured/FeaturedMatches.js
+++ src/js/components/featured/FeaturedMatches.js
@@ -5,19 +5,32 @@ import Champion from "./Champions"
 export default class FeaturedMatches extends React.Component {
     constructor(props) {
       super()
-      console.log(this);
-      console.log(_.filter(props.bannedChampions, {teamId: 100}));
-      console.log(_.filter(props.participants, {teamId: 100}));
     }
 
 
     render() {
-      // const blueTeam = {
-      //   bannedChampions: _.filter(this.props.bannedChampions, {teamId: 100}),
-      //   participants:
-      // }
+      const blueTeam = {
+        bannedChampions: _.filter(this.props.bannedChampions, {teamId: 100}),
+        participants: _.filter(this.props.participants, {teamId: 100})
+      }
+      const redTeam = {
+        bannedChampions: _.filter(this.props.bannedChampions, {teamId: 200}),
+        participants: _.filter(this.props.participants, {teamId: 200})
+      }
+      const BlueTeamChampions = blueTeam.participants.map((champion)=>{
+        <Champion key={champion.championId + champion.teamId} {...champion}/>
+      })
+      const RedTeamChampions = redTeam.participants.map((champion)=>{
+        <Champion key={champion.championId + champion.teamId} {...champion}/>
+      })
       return(
           <div>
+            <div>
+              {BlueTeamChampions}
+            </div>
+            <div>
+              {RedTeamChampions}
+            </div>
           </div>
       )
     }
